c1edd984e394a43f81bf54095eddddd1c3540c56
vefa <44091688+veefa@users.noreply.github.com>
2025-10-13 15:20:05 +0100

    Clean up files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import postcssImport from "postcss-import";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
-})
+  css: {
+    postcss: {
+      plugins: [postcssImport(), autoprefixer()],
+    },
+  },
+});
